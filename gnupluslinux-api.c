591ecbef492083509939d5962ac9d7076c6917fa
leonardoDomper <leonardonunes169@gmail.com>
2021-08-14 20:41:29 -0300

    update

--- gnupluslinux-api.c
+++ gnupluslinux-api.c
@@ -19,11 +19,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "gnupluslinux-api.h" 
 #include "web-scraping/scraping.h"
-
-#define URL "https://gnupluslinux.com/anime/"
-
-#define CURL_CMD "curl --output source.png "
 
 int lines(char *str) {
   	if (*str == '\0') return 0;
@@ -31,57 +28,38 @@ int lines(char *str) {
 	return lines(str); 
 }
 
-uint8_t get_img(WSCONF cnfg, char *url, size_t i) {
-  	get_file(url);
-
-  	char *srcstr = get_source(cnfg);
-  	if (!srcstr) return 1;
-
-  	int sizestr = lines(srcstr);
-  	if (i > sizestr) return 2;
-
-  	size_t cnt = 0;
-  	while (cnt != i) if (*srcstr++ == '\n') cnt++;
-
-  	Queue *flurl = create_queue();
-  	while (*srcstr != '\n') push_queue(flurl,*srcstr++);
-
-  	char *str = get_str(flurl->f); 
-  	char *cmd;
-  	cmd = (char *) malloc((
-    		strlen(CURL_CMD) +
-    		strlen(url) + 
-    		strlen(str)) * sizeof(char));
-  	strcpy(cmd,CURL_CMD);
-  	strcat(cmd,url);
-  	strcat(cmd,str);
-
-  	printf("%s\n", cmd);
-  	system(cmd);
-
-  	free(cmd);
-  	free(flurl);
-  	return 0;
-}
-
-int main(int argc, char *argv[]) {
-  	WSCONF cnfg;
-
+char *get_anime_file(char *fol, size_t i) {
+	WSCONF cnfg;
   	cnfg.string_init = "alt=\"[IMG]\"> <a href=";
   	cnfg.string_end = "\">";
   	cnfg.enable_print = 0;
 
-  	char *url = malloc((strlen(argv[1]) + strlen(URL) + 1) * sizeof(char));
-  	strcpy(url,URL);
-  	strcat(url,argv[1]);
-  	if (argv[1][(strlen(argv[1]) - 1)] != '/') 
-    		strcat(url,"/");
+  	char *fol_url = malloc(strlen(URL) + (strlen(fol)) * sizeof(char));
+  	strcpy(fol_url,URL);
+  	strcat(fol_url,fol);
+  	if (fol[(strlen(fol)-1)] != '/') strcat(fol_url,"/");
 
-  	int i = strtol(argv[2],NULL,10);
-  	uint8_t img = get_img(cnfg,url,i);
-  	if (img == 1) printf("Error in get source\n");
-  	else if (img == 2) printf("Invalid index\n");
+  	get_file(fol_url);
 
-  	free(url);
-  	return 0;
+  	char *src_str = get_source(cnfg);
+  	if (!src_str) return NULL;
+
+  	int size_str = lines(src_str);
+  	if (i > size_str) return NULL;
+
+  	size_t cnt = 0;
+  	while (cnt != i) if (*src_str++ == '\n') cnt++;
+
+  	Queue *fl_url = create_queue();
+  	while (*src_str != '\n') push_queue(fl_url,*src_str++);
+
+  	char *str = get_str(fl_url->f); 
+  	char *url = (char *) malloc((strlen(fol_url) + strlen(str)) * sizeof(char));
+  	strcat(url,fol_url);
+
+  	free(fol_url);
+
+  	strcat(url,str);
+
+  	return url;
 }
---
+++ gnupluslinux-api.h
@@ -0,0 +1,27 @@
+/*
+ * https://gnupluslinux.com/anime Api
+ * Copyright © 2021 Leonardo Zamboni 🌎 leozamboni.dev
+ *
+ * this program is free software: you can redistribute it and/or modify
+ * it under the terms of the gnu general public license as published by
+ * the free software foundation, either version 3 of the license, or
+ * (at your option) any later version.
+ * 
+ * this program is distributed in the hope that it will be useful,
+ * but without any warranty; without even the implied warranty of
+ * merchantability or fitness for a particular purpose.  see the
+ * gnu general public license for more details.
+ * 
+ * you should have received a copy of the gnu general public license
+ * along with this program.  if not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef GNUPLUSLINUX_API_H
+#define GNUPLUSLINUX_API_H
+
+#include <stdlib.h>
+
+#define URL "https://gnupluslinux.com/anime/"
+
+char *get_anime_file(char *folder, size_t index);
+
+#endif
